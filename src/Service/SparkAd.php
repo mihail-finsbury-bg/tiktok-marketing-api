c0b3785ecba12f5a1853387dad0e98dea0b3d852
Mihail Ivanov <mihail.ivanov@finsburymedia.com>
2023-02-08 17:29:37 +0200

    * spark post list

--- src/Service/SparkAd.php
+++ src/Service/SparkAd.php
@@ -9,7 +9,7 @@ use Promopult\TikTokMarketingApi\AbstractService;
 final class SparkAd extends AbstractService
 {
     /**
-     * Get spark ad post information
+     * Get info about a Spark Ad post
      *
      * Use this endpoint to get the information about the Spark Ads post that you have been authorized to use as an ad. 
      * You need to pass in the authorization code from the post owner.
@@ -33,6 +33,37 @@ final class SparkAd extends AbstractService
             [
                 'advertiser_id' => $advertiserId,
                 'auth_code' => $authCode
+            ]
+        );
+    }
+
+    /**
+     * Get Spark Ad posts
+     *
+     * Use this endpoint to get a list of Spark Ads posts that have been authorized to an ad account.
+     *
+     * @param int $advertiserId Advertiser ID
+     * @param int|null $page   The current page number. The default value is 1. The value must be ≥ 1.
+     * @param int|null $pageSize   The page size. The default value: 20. The value must be in the range of 1-50.
+     *
+     * @return array
+     *
+     * @throws \Throwable
+     * 
+     * @see https://ads.tiktok.com/marketing_api/docs?id=1738376465972226
+     */
+    public function list(
+        int $advertiserId,
+        ?int $page = null,
+        ?int $pageSize = null
+    ): array {
+        return $this->requestApi(
+            'GET',
+            '/open_api/v1.3/tt_video/list/',
+            [
+                'advertiser_id' => $advertiserId,
+                'page' => $page,
+                'page_size' => $pageSize,
             ]
         );
     }
